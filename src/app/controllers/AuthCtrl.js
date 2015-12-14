d340ad82f45fa17e394bfcbc8403a141d7c3086d
Dan Dart <dandart@googlemail.com>
2015-12-14 00:16:44 +0000

    Fix User model and register controller

--- src/app/controllers/AuthCtrl.js
+++ src/app/controllers/AuthCtrl.js
@@ -11,37 +11,40 @@ export async function LoginCtrl(req, res) {
         return res.status(401).send({error: 'Invalid Cdedentials'});
     
     req.session.user = user;
-
-    return res.status(204);
+    
+    return res.status(204).send('');
 };
 
-export async function RegisterCtrl(req, res) {
-    let user = await User.findOne({username: req.post.username});
-    
-    if (user)
+export async function RegisterCtrl(req, res) {    
+    let user = await User.findOne({username: req.body.username});
+        
+    if (user) {
         return res.status(400).send({error:'User exists'});
+    }
     
-    user = new User();
-    user.username = req.body.username;
-    user.password = passwordhash.generate(
-        req.body.password,
-        {
-            algorithm: 'sha512'
-        }
-    );
-
+    user = new User({
+        username: req.body.username,
+        password: passwordhash.generate(
+            req.body.password,
+            {
+                algorithm: 'sha512'
+            }
+        )
+    });
+            
     try {
         await user.save();
     } catch(err) {
+        console.log(err);
         return res.status(500).send({error: 'Error saving new user'});
     }
 
-    return res.status(201);
+    return res.status(201).send('');
 };
 
 export function LogoutCtrl(req, res) {
     req.session.reset();
-    return res.status(204);
+    return res.status(204).send('');
 };
 
 export function StatusCtrl(req, res) {
