99d9fc0cb2dfa08a8e6be0cef540c65435ba0b2c
Dan <git@dandart.co.uk>
2016-01-01 00:43:23 +0000

    try to add steam and gnusocial but steam does not save

---
+++ src/app/lib/passport/steam.js
@@ -0,0 +1,54 @@
+import config from '../../../config/app';
+import passport from 'passport';
+import {Strategy as SteamStrategy} from 'passport-steam';
+import User from '../model/user';
+import {requireLogin} from '../filters';
+
+export default class Steam {
+    setup(req, res, next) {
+        passport.use(new SteamStrategy(
+            config.oauth.steam,
+            async (identifier, profile, done) => {
+                let user = await User.findById(req.session.user._id);
+                user.networks = user.networks || {};
+                user.networks[profile.id] = {
+                    type: 'steam',
+                    name: profile.displayName,
+                    identifier
+                };
+                try {
+                    // This doesn't work!
+                    await user.save();
+                } catch (err) {
+                    console.log(err);
+                }
+                req.session.user = user;
+                done(null, {});
+            }
+        ));
+        
+        next();
+    }
+
+    addRoutesTo(app) {
+        app.get('/auth/steam',
+        requireLogin,
+            this.setup,
+            passport.authenticate(
+                'steam'
+            )
+        );
+        
+        app.get('/auth/steam/callback',
+            requireLogin,
+            this.setup,
+            passport.authenticate(
+                'steam',
+                {
+                    successRedirect: '/#/settings',
+                    failureRedirect: '/#/settings'
+                }
+            )
+        );
+    }
+}
