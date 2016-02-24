deef99978014f1f602b2e36f1a4bc2d46805548c
Dan Dart <dandart@googlemail.com>
2016-02-24 23:38:12 +0000

    add a simple imap inbox via simple-imap-inbox (requires fix PRd to them)

--- src/app/lib/network/email.js
+++ src/app/lib/network/email.js
@@ -1,12 +1,34 @@
+import Inbox from 'simple-imap-inbox';
+
 export default class Email {
-    constructor(options) {
+    constructor(objNetwork) {
+        this.objNetwork = objNetwork;
     }
 
-    getInbox() {
-
+    async getInbox() {
+        try {
+            let inbox = new Inbox({
+                user: this.objNetwork.name,
+                password: this.objNetwork.password,
+                host: this.objNetwork.imap,
+                port: 993,
+                tls: true
+            });
+            await inbox.connect(true);
+            let messages = await inbox.fetch(10);
+            return messages.map((message)=>({
+                from: message.headers.from[0],
+                subject: message.headers.subject[0],
+                date: message.headers.date[0],
+                id: message.attributes.uid
+            }));
+        } catch (err) {
+            console.log('caught here', err);
+            return [];
+        }
     }
 
     async getFeed() {
-
+        return [];
     }
 }
