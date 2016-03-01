53978c365375127ea527a2deabf5e20fd8bcf7bf
Dan <git@dandart.co.uk>
2016-03-01 19:46:30 +0000

    inbox improvements and static assets management inc favicon

--- src/public/js/lib/globals.js
+++ src/public/js/lib/globals.js
@@ -1,5 +1,20 @@
 export default ($) => {
     $(document).ready(() => {
         // Insert things you can't do with Angular here
+
+        $('.messages').height(
+            $('body').height() - $('footer').height()
+        );
+
+        $('input.select-all[type="checkbox"]').on('change', (ev) => {
+            let $this = $(ev.currentTarget);
+
+            $this.parents('table')
+                .find('input[type="checkbox"]')
+                .prop(
+                    'checked',
+                    $this.prop('checked')
+                );
+        })
     });
 }
