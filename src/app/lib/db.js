db41a86da9b7cd80cc8d42e0082c44066c55e63e
Dan Dart <github@dandart.co.uk>
2016-06-22 22:15:29 +0100

    use native promises and make feed produce rss properly

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,3 +1,5 @@
 import mongoose from 'mongoose';
 export default (dbhost) =>
     mongoose.connect('mongodb://'+dbhost+'/pcomm');
+
+mongoose.Promise = Promise;
--- src/app/lib/network/rss.js
+++ src/app/lib/network/rss.js
@@ -22,28 +22,36 @@ export default class RSS {
     }
 
     async getFeed() {
-        /*
-        let req = request(this.objNetwork.url),
-            feedparser = new FeedParser();
+        return await new Promise((res,rej) => {
+            let items = [],
+                req = request(this.objNetwork.url),
+                feedparser = new FeedParser();
 
-        req.on('error', (err) => console.log(err));
-        req.on('response', function(res) {
-            let stream = this;
-            if (200 != res.statusCode) return this.emit('error', new Error('Bad status code'));
-            stream.pipe(feedparser);
+            req.on('error', (err) => console.log(err));
+            req.on('response', function(res) {
+                let stream = this;
+                if (200 != res.statusCode) return rej('Status code not 200');
+                stream.pipe(feedparser);
+            });
+
+            feedparser.on('error', (err) => rej(err));
+            feedparser.on('readable', function() {
+                let stream = this,
+                    meta = this.meta,
+                    item;
+                while (item = stream.read()) {
+                    items.push({
+                        from: item.author,
+                        subject: item.title,
+                        date: moment(new Date(item.date)).format('lll'),
+                        id: item.guid,
+                        body: item.description,
+                        link: item.link
+                    });
+                }
+            });
+            feedparser.on('end', () => res(items));
         });
-
-        feedparser.on('error', (err) => console.log(err));
-        feedparser.on('readable', function() {
-            let stream = this,
-                meta = this.meta,
-                item;
-            while (item = stream.read()) {
-
-            }
-        }
-        */
-        return [];
     }
 
     getLiveMessages() {
